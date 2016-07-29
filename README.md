976180535a71c0566ba536e52ba9caa53e18a20a
David Perfors <dnperfors@gmail.com>
2016-07-29 22:28:48 +0200

    Prepare for release 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # CodeStyleAnalyzer
 CodeStyleAnalyzer is a Roslyn analyzer and code fixer to check for coding style violations. The coding styles are [documented here][corefx-coding-style].
 
+Large part of the code is based on the [.NET CodeFormatter tool][codeformatter]
+
 [corefx-coding-style]: http://github.com/dotnet/corefx/blob/master/Documentation/coding-guidelines/coding-style.md
+[codeformatter]: https://github.com/dotnet/codeformatter
--- src/CodeStyleAnalyzer/CodeStyleAnalyzer/Properties/AssemblyInfo.cs
+++ src/CodeStyleAnalyzer/CodeStyleAnalyzer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CodeStyleAnalyzer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Analyzer to check code style against the .NET core coding style guide.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CodeStyleAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright David Perfors ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
